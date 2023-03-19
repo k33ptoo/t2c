9eb42c5f2c281d5a3538b52eb372f19614400b16
Amos Chepchieng <akchieng@gmail.com>
2023-03-19 04:26:30 +0300

    code cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="">
   <div class="bg-[#1B1D22] p-4 md:p-2 flex h-screen w-screen justify-items-start flex-col items-center gap-10">
-    <div class="md:w-1/2"><img src="./assets/logo.png" alt="logo" class="md:w-1/12 w-1/6"></div>
+    <div class="md:w-1/2 flex justify-start w-full"><img src="./assets/logo.png" alt="logo" class="md:w-1/12 w-2/12"></div>
     <Res />
   </div>
 </template>
